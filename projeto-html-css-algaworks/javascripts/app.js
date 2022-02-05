a883ace438e51324b60c11ebe4cfe98f7fdb8174
CarlosG2304 <carlosgmr2304@gmail.com>
2022-02-05 10:43:36 -0300

    Projeto com html e css da algaworks

---
+++ projeto-html-css-algaworks/javascripts/app.js
@@ -0,0 +1,6 @@
+var altenador = document.querySelector('.js-botao-chaveador')
+
+altenador.onclick = function() {
+ var menu = document.querySelector('.js-menu');
+ menu.classList.toggle('menu--exibindo') 
+}
